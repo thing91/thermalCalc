1ad1574dbf0d582398ea88e3c3216e6bc22dea82
krzysztof natusiewicz <kristo91@gmail.com>
2018-03-06 04:37:33 +0100

    final version of calculation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import matplotlib.pyplot as plt
+from pylab import plot, show, close
 
 altitudeArr = (0,2,4,6,8,10,
                12,14,16,18,20,
@@ -8,29 +10,47 @@ ambientTempArr = (287,276,265,250,237,224,
                   218,218,218,218,218,
                   219,220,222,224,226,
                   228,233,238,244,248)      #[K]
-interval = 10                               #[min]
-startTemp = 271                             #[K]
+flightUpspeedRate = 1/10                    #[h/km]
+
+timePer2km = 12                             #[min]
 isolCondCoeff = 0.033                       #[W/(m*K)]
 surface = 0.072                             #[m^2]
-thickness = 0.02                            #[m]
+thickness = 0.05                             #[m]
 airSpecificHeat = 1005                      #[J/(kg*K)]
 airVolume = 0.001                           #[m^3]
 airDensity = 1.2                            #[kg/m^3]
 
-startHeat = airDensity*airSpecificHeat*airVolume
-
 transferredHeatConstant = (isolCondCoeff*surface)/thickness
-temp = ambientTempArr[0]
+airMass = airDensity*airVolume
 
-def calcTransferredHeatRate(i):
-    transferedHeatRate = (temp - ambientTempArr[i])*transferredHeatConstant
+def calcTransferredHeatRate(currentTemp, ambientTemp):
+    transferedHeatRate = (currentTemp - ambientTemp)*transferredHeatConstant
+    print("transferHeatRate = {}".format(transferedHeatRate))
     return transferedHeatRate
-def calcInteriorTempChange(heatRate, time, temp):
+
+def calcInteriorTempChange(heatRate, time, currentTemp):
     heatTransfer = heatRate * time
-    changedTemp = heatTransfer/(airSpecificHeat*airDensity*airVolume) - temp
+    changedTemp = currentTemp - heatTransfer/(airSpecificHeat*airMass)
     return changedTemp
 
-
 def main():
+    startHeat = airDensity*airSpecificHeat*airVolume
+    temp = ambientTempArr[0]
+    tempVal = []
+    tempVal.append(temp)
+    altitude = altitudeArr[0]
+    i = 1
+    while (altitude < 39):
+        heatRate = calcTransferredHeatRate(temp, ambientTempArr[i])
+        temp = calcInteriorTempChange(heatRate, timePer2km, tempVal[(i-1)])
+        tempVal.append(temp)
+        altitude = altitudeArr[i]
+        i+=1
     
+    plt.plot(tempVal,altitudeArr)
+    plt.plot(ambientTempArr,altitudeArr)
+    plt.ylabel("label")
+    plt.show()
     
+if __name__== "__main__":
+    main()    
